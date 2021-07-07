8d1acc8935ed323c3afb1487ba5e3ef34306114b
syabontech <syabontech@gmail.com>
2021-07-07 22:46:42 +0900

    Create DB

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module example.com/HelloGo
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/jinzhu/gorm v1.9.16
+	github.com/mattn/go-sqlite3 v1.14.7
+)
--- main.go
+++ main.go
@@ -2,14 +2,96 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Todo struct{
+	gorm.Model
+	Text string
+	Status string
+}
+
+//DB初期化
+func dbInit(){
+	db, err := gorm.Open("sqlite3", "test.sqlite3") 
+	if err != nil {
+		panic("データベース開けず！(dbInit)")
+	}
+
+	db.AutoMigrate(&Todo{})
+	defer db.Close()
+}
+
+//DB追加
+func dbInsert(text string, status string){
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("データベース開けず!(dbInsert)")
+	}
+	db.Create(&Todo{Text: text, Status: status})
+	defer db.Close()
+}
+
+//DB更新
+func dbUpdate(id int, text string, status string){
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("データベース開けず!(dbUpdate)")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+	db.Close()
+}
+
+//DB削除
+func dbDelete(id int){
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil{
+		panic("データベース開けず!(dbDelete)")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
+	db.Close()
+}
+
+//DB全取得
+func dbGetAll() []Todo{
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil{
+		panic("データベース開けず!(dbGetAll())")
+	}
+	var todos []Todo
+	db.Order("created_at desc").Find(&todos)
+	db.Close()
+	return todos
+}
+
+//DB一つ取得
+func dbGetOne(id int) Todo{
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil{
+		panic("データベース開けず!(dbGetOne())")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	db.Close()
+	return todo
+}
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
+	data := "Hello Go/Gin"
+
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		ctx.HTML(200, "index.html", gin.H{"data": data})
 	})
 
 	router.Run()
